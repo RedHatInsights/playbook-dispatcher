c566442d03a8bac4e23c482338e8fe2080062519
Jozef Hartinger <jharting@redhat.com>
2021-02-15 17:02:42 +0100

    RHCLOUD-11962 extend Runner events processing

---
+++ internal/common/ansible/runner.go
@@ -0,0 +1,33 @@
+package ansible
+
+import (
+	messageModel "playbook-dispatcher/internal/common/model/message"
+	"playbook-dispatcher/internal/common/utils"
+	"sort"
+)
+
+func GetAnsibleHosts(events []messageModel.PlaybookRunResponseMessageYamlEventsElem) []string {
+	hosts := make(map[string]interface{})
+
+	for _, event := range events {
+		if event.EventData != nil && event.EventData.Host != nil {
+			hosts[*event.EventData.Host] = true
+		}
+	}
+
+	keys := utils.MapKeys(hosts)
+	sort.Strings(keys)
+	return keys
+}
+
+func GetStdout(events []messageModel.PlaybookRunResponseMessageYamlEventsElem) (result string) {
+	sort.SliceStable(events, func(i, j int) bool {
+		return events[i].Counter < events[j].Counter
+	})
+
+	for _, event := range events {
+		result += event.Stdout
+	}
+
+	return
+}
---
+++ internal/common/ansible/runner_test.go
@@ -0,0 +1,97 @@
+package ansible
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	messageModel "playbook-dispatcher/internal/common/model/message"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func loadFile(path string) (events []messageModel.PlaybookRunResponseMessageYamlEventsElem) {
+	file, err := ioutil.ReadFile(path)
+	Expect(err).ToNot(HaveOccurred())
+
+	lines := strings.Split(string(file), "\n")
+
+	for _, line := range lines {
+		if len(strings.TrimSpace(line)) == 0 {
+			continue
+		}
+
+		event := messageModel.PlaybookRunResponseMessageYamlEventsElem{}
+		err = json.Unmarshal([]byte(line), &event)
+		Expect(err).ToNot(HaveOccurred())
+
+		events = append(events, event)
+	}
+
+	return events
+}
+
+var _ = Describe("Ansible", func() {
+	Describe("host", func() {
+		It("determines ansible host from a successful run", func() {
+			events := loadFile("./test-events1.jsonl")
+			hosts := GetAnsibleHosts(events)
+			Expect(hosts).To(HaveLen(1))
+			Expect(hosts[0]).To(Equal("localhost"))
+		})
+
+		It("determines ansible host from a failed run", func() {
+			events := loadFile("./test-events2.jsonl")
+			hosts := GetAnsibleHosts(events)
+			Expect(hosts).To(HaveLen(1))
+			Expect(hosts[0]).To(Equal("localhost"))
+		})
+
+		It("determines ansible host from an incomplete run", func() {
+			events := loadFile("./test-events3.jsonl")
+			hosts := GetAnsibleHosts(events)
+			Expect(hosts).To(HaveLen(1))
+			Expect(hosts[0]).To(Equal("localhost"))
+		})
+
+		It("determines ansible host from an incomplete run (2)", func() {
+			events := loadFile("./test-events4.jsonl")
+			hosts := GetAnsibleHosts(events)
+			Expect(hosts).To(HaveLen(0))
+		})
+
+		It("determines ansible hosts from a multi-host run", func() {
+			events := loadFile("./test-events5.jsonl")
+			hosts := GetAnsibleHosts(events)
+			Expect(hosts).To(HaveLen(2))
+			Expect(hosts[0]).To(Equal("jharting1"))
+			Expect(hosts[1]).To(Equal("jharting2"))
+		})
+	})
+
+	Describe("stdout", func() {
+		It("determines stdout from a successful run", func() {
+			events := loadFile("./test-events1.jsonl")
+			stdout := GetStdout(events)
+			Expect(stdout).To(Equal("\r\nPLAY [ping] ********************************************************************\r\nTASK [ping] ********************************************************************\x1b[0;32mok: [localhost]\x1b[0m\r\nPLAY RECAP *********************************************************************\r\n\x1b[0;32mlocalhost\x1b[0m                  : \x1b[0;32mok=1   \x1b[0m changed=0    unreachable=0    failed=0    skipped=0    rescued=0    ignored=0   \r\n"))
+		})
+
+		It("determines stdout from a failed run", func() {
+			events := loadFile("./test-events2.jsonl")
+			stdout := GetStdout(events)
+			Expect(stdout).To(Equal("\r\nPLAY [ping] ********************************************************************\r\nTASK [fail] ********************************************************************\x1b[0;31mfatal: [localhost]: FAILED! => {\"changed\": false, \"msg\": \"Always fail\"}\x1b[0m\r\nPLAY RECAP *********************************************************************\r\n\x1b[0;31mlocalhost\x1b[0m                  : ok=0    changed=0    unreachable=0    \x1b[0;31mfailed=1   \x1b[0m skipped=0    rescued=0    ignored=0   \r\n"))
+		})
+
+		It("determines stdout from an incomplete run", func() {
+			events := loadFile("./test-events3.jsonl")
+			stdout := GetStdout(events)
+			Expect(stdout).To(Equal("\r\nPLAY [ping] ********************************************************************\r\nTASK [ping] ********************************************************************"))
+		})
+
+		It("determines stdout from an incomplete run (2)", func() {
+			events := loadFile("./test-events4.jsonl")
+			stdout := GetStdout(events)
+			Expect(stdout).To(Equal(""))
+		})
+	})
+})
