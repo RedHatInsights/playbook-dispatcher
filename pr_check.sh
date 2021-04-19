211586e4578aed001659443edbd7f872f13f0e57
Brandon Squizzato <bsquizza@redhat.com>
2021-04-19 14:06:37 -0400

    Remove separate ephemeral secrets template

--- pr_check.sh
+++ pr_check.sh
@@ -11,30 +11,19 @@ IQE_PLUGINS="rhc"
 IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION="playbook_dispatcher"
 
+
 # Install bonfire repository/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
-source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 # Build Playbook Dispatcher image based on the latest commit
-source build.sh
+source $CICD_ROOT/build.sh
+
+# Execute unit tests
+#source $APP_ROOT/unit_test.sh
 
 # Deploy the new image to an ephemeral environment
-source _common_deploy_logic.sh
-result=$(bonfire config deploy \
-    --ref-env insights-stage \
-    --app $APP_NAME \
-    --set-template-ref $COMPONENT_NAME=$GIT_COMMIT \
-    --set-template-ref $TEST_SECRETS_COMPONENT_NAME=$GIT_COMMIT \
-    --set-image-tag $IMAGE=$IMAGE_TAG \
-    --get-dependencies)
-
-if [ $? -eq 0 ]; then
-    export NAMESPACE=$result
-fi
-
-# Deploy Ingress services to the ephemeral environment
-bonfire config deploy --ref-env insights-stage --app ingress --get-dependencies --namespace $NAMESPACE
+source $CICD_ROOT/deploy_ephemeral_env.sh
 
 # Deploy an IQE pod and run the smoke tests
-source smoke_test.sh
+source $CICD_ROOT/smoke_test.sh
