0605cff8d18267c64f1ed5217a6066d8155f4b59
Jozef Hartinger <jharting@redhat.com>
2022-02-09 09:48:47 +0100

    define default value using gorm tag

--- internal/common/model/db/Run.go
+++ internal/common/model/db/Run.go
@@ -19,7 +19,7 @@ const (
 type Run struct {
 	ID      uuid.UUID `gorm:"type:uuid"`
 	Account string
-	OrgID   string
+	OrgID   string `gorm:"default:unknown"`
 	Service string `gorm:"default:unknown"`
 
 	Recipient     uuid.UUID `gorm:"type:uuid"`
