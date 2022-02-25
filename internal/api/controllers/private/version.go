824b328d7196a902800a5c0c0243818f7147d1ba
CodyWMitchell <comitche@redhat.com>
2022-02-25 10:00:34 -0500

    Pull build.commit from config

--- internal/api/controllers/private/version.go
+++ internal/api/controllers/private/version.go
@@ -2,10 +2,13 @@ package private
 
 import (
 	"net/http"
+	"playbook-dispatcher/internal/common/config"
 
 	"github.com/labstack/echo/v4"
 )
 
 func (this *controllers) ApiInternalVersion(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, Version("v2"))
+	cfg := config.Get()
+
+	return ctx.JSON(http.StatusOK, Version(cfg.GetString("build.commit")))
 }
