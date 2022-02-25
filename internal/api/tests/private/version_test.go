fd5ea274899a1cd0d6eb7f8c7bac5e3707904151
CodyWMitchell <comitche@redhat.com>
2022-02-25 18:25:35 -0500

    Add test for version endpoint

---
+++ internal/api/tests/private/version_test.go
@@ -0,0 +1,40 @@
+package private
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"playbook-dispatcher/internal/common/config"
+	"playbook-dispatcher/internal/common/utils/test"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Version", func() {
+	Describe("get internal version", func() {
+		It("should return the git revision the API was built from", func() {
+
+			req, err := http.NewRequest(http.MethodGet, "http://localhost:8000/internal/version", nil)
+			Expect(err).ToNot(HaveOccurred())
+
+			res, err := test.Client.Do(req)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(http.StatusOK))
+
+			data, err := ioutil.ReadAll(res.Body)
+			Expect(err).ToNot(HaveOccurred())
+
+			cfg := config.Get()
+			version := cfg.GetString("build.commit")
+
+			// Remove quotes and newline from the returned data
+			data = bytes.Replace(data, []byte("\""), []byte(""), 2)
+			data = bytes.Replace(data, []byte("\n"), []byte(""), 1)
+
+			Expect(string(data)).To(BeEquivalentTo(version))
+
+		})
+	})
+})
