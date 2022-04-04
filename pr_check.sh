d173a5897919394289c61acae7565c6fd70fe1bb
Rodrigo Antunes <rantunes@redhat.com>
2022-04-04 13:23:01 -0300

    Run RHC contract tests as part of the PR Check job

--- pr_check.sh
+++ pr_check.sh
@@ -5,12 +5,8 @@
 APP_NAME="playbook-dispatcher"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="playbook-dispatcher"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/playbook-dispatcher"
-
-IQE_PLUGINS="playbook-dispatcher"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION=""
 IQE_CJI_TIMEOUT="30m"
-
+REF_ENV="insights-stage"
 
 # Install bonfire repository/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -25,5 +21,24 @@ source $CICD_ROOT/build.sh
 # Deploy the new image to an ephemeral environment
 source $CICD_ROOT/deploy_ephemeral_env.sh
 
-# Deploy an IQE pod and run the smoke tests
+# Run Playbook Dispatcher isolated tests
+IQE_PLUGINS="playbook-dispatcher"
+IQE_MARKER_EXPRESSION="smoke"
+source $CICD_ROOT/cji_smoke_test.sh
+
+# Re-deploy Playbook Dispatcher to an ephemeral environment, this time enabling the communication with Cloud Connector
+bonfire deploy \
+    ${APP_NAME} \
+    --source=appsre \
+    --ref-env ${REF_ENV} \
+    --set-template-ref ${COMPONENT_NAME}=${GIT_COMMIT} \
+    --set-image-tag ${IMAGE}=${IMAGE_TAG} \
+    --namespace ${NAMESPACE} \
+    --timeout ${DEPLOY_TIMEOUT} \
+    --set-parameter playbook-dispatcher/CLOUD_CONNECTOR_IMPL=impl
+
+# Run RHC Contract integration tests
+COMPONENT_NAME="cloud-connector"
+IQE_PLUGINS="rhc-contract"
+IQE_IMAGE_TAG="rhc-contract"
 source $CICD_ROOT/cji_smoke_test.sh
