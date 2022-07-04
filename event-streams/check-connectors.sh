1b34d4b346f525dceac8f1906018c5161898d386
Jozef Hartinger <jharting@redhat.com>
2022-07-04 09:12:22 +0200

    make connect host:port fully configurable in the check-host script

--- event-streams/check-connectors.sh
+++ event-streams/check-connectors.sh
@@ -4,4 +4,4 @@
 
 curl -s "http://${CONNECT_HOST:-localhost}:${CONNECT_PORT:-8083}/connectors?expand=status" | \
   jq -c -M 'map({name: .status.name } +  {tasks: .status.tasks}) | .[] | {task: ((.tasks[]) + {name: .name})}  | select(.task.state=="FAILED") | {name: .task.name, task_id: .task.id|tostring} | ("/connectors/"+ .name + "/tasks/" + .task_id + "/restart")' | \
-  xargs -I{connector_and_task} curl -v -X POST "http://localhost:8083"\{connector_and_task\}
+  xargs -I{connector_and_task} curl -v -X POST "http://${CONNECT_HOST:-localhost}:${CONNECT_PORT:-8083}"\{connector_and_task\}
