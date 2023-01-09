2070762a2f17f7e8b6d7932089a62c1946959eb9
Stephen <stephen.adams@redhat.com>
2023-01-09 10:46:06 -0500

    Extend the event interface for hosts (#251)
    * Extend the event interface for hosts

---
+++ event-streams/src/main/java/com/redhat/cloud/platform/playbook_dispatcher/types/RunHostPayload.java
@@ -0,0 +1,345 @@
+
+package com.redhat.cloud.platform.playbook_dispatcher.types;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "run_id",
+    "inventory_id",
+    "host",
+    "stdout",
+    "status",
+    "created_at",
+    "updated_at"
+})
+public class RunHostPayload {
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    private String id;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("run_id")
+    private String runId;
+    @JsonProperty("inventory_id")
+    private String inventoryId;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("host")
+    private String host;
+    @JsonProperty("stdout")
+    private String stdout;
+    /**
+     * 
+     * (Required)
+     * (Required)
+     * 
+     */
+    @JsonProperty("status")
+    private RunHostPayload.Status status;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("created_at")
+    private String createdAt;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("updated_at")
+    private String updatedAt;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("run_id")
+    public String getRunId() {
+        return runId;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("run_id")
+    public void setRunId(String runId) {
+        this.runId = runId;
+    }
+
+    @JsonProperty("inventory_id")
+    public String getInventoryId() {
+        return inventoryId;
+    }
+
+    @JsonProperty("inventory_id")
+    public void setInventoryId(String inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("host")
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("host")
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    @JsonProperty("stdout")
+    public String getStdout() {
+        return stdout;
+    }
+
+    @JsonProperty("stdout")
+    public void setStdout(String stdout) {
+        this.stdout = stdout;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("status")
+    public RunHostPayload.Status getStatus() {
+        return status;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("status")
+    public void setStatus(RunHostPayload.Status status) {
+        this.status = status;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("created_at")
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("created_at")
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("updated_at")
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("updated_at")
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(RunHostPayload.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("runId");
+        sb.append('=');
+        sb.append(((this.runId == null)?"<null>":this.runId));
+        sb.append(',');
+        sb.append("inventoryId");
+        sb.append('=');
+        sb.append(((this.inventoryId == null)?"<null>":this.inventoryId));
+        sb.append(',');
+        sb.append("host");
+        sb.append('=');
+        sb.append(((this.host == null)?"<null>":this.host));
+        sb.append(',');
+        sb.append("stdout");
+        sb.append('=');
+        sb.append(((this.stdout == null)?"<null>":this.stdout));
+        sb.append(',');
+        sb.append("status");
+        sb.append('=');
+        sb.append(((this.status == null)?"<null>":this.status));
+        sb.append(',');
+        sb.append("createdAt");
+        sb.append('=');
+        sb.append(((this.createdAt == null)?"<null>":this.createdAt));
+        sb.append(',');
+        sb.append("updatedAt");
+        sb.append('=');
+        sb.append(((this.updatedAt == null)?"<null>":this.updatedAt));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.createdAt == null)? 0 :this.createdAt.hashCode()));
+        result = ((result* 31)+((this.stdout == null)? 0 :this.stdout.hashCode()));
+        result = ((result* 31)+((this.inventoryId == null)? 0 :this.inventoryId.hashCode()));
+        result = ((result* 31)+((this.host == null)? 0 :this.host.hashCode()));
+        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
+        result = ((result* 31)+((this.runId == null)? 0 :this.runId.hashCode()));
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        result = ((result* 31)+((this.status == null)? 0 :this.status.hashCode()));
+        result = ((result* 31)+((this.updatedAt == null)? 0 :this.updatedAt.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof RunHostPayload) == false) {
+            return false;
+        }
+        RunHostPayload rhs = ((RunHostPayload) other);
+        return ((((((((((this.createdAt == rhs.createdAt)||((this.createdAt!= null)&&this.createdAt.equals(rhs.createdAt)))&&((this.stdout == rhs.stdout)||((this.stdout!= null)&&this.stdout.equals(rhs.stdout))))&&((this.inventoryId == rhs.inventoryId)||((this.inventoryId!= null)&&this.inventoryId.equals(rhs.inventoryId))))&&((this.host == rhs.host)||((this.host!= null)&&this.host.equals(rhs.host))))&&((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id))))&&((this.runId == rhs.runId)||((this.runId!= null)&&this.runId.equals(rhs.runId))))&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))))&&((this.status == rhs.status)||((this.status!= null)&&this.status.equals(rhs.status))))&&((this.updatedAt == rhs.updatedAt)||((this.updatedAt!= null)&&this.updatedAt.equals(rhs.updatedAt))));
+    }
+
+    public enum Status {
+
+        RUNNING("running"),
+        SUCCESS("success"),
+        FAILURE("failure"),
+        TIMEOUT("timeout"),
+        CANCELED("canceled");
+        private final String value;
+        private final static Map<String, RunHostPayload.Status> CONSTANTS = new HashMap<String, RunHostPayload.Status>();
+
+        static {
+            for (RunHostPayload.Status c: values()) {
+                CONSTANTS.put(c.value, c);
+            }
+        }
+
+        private Status(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value;
+        }
+
+        @JsonValue
+        public String value() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static RunHostPayload.Status fromValue(String value) {
+            RunHostPayload.Status constant = CONSTANTS.get(value);
+            if (constant == null) {
+                throw new IllegalArgumentException(value);
+            } else {
+                return constant;
+            }
+        }
+
+    }
+
+}
