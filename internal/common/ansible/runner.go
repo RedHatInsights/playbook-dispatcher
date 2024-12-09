06f7c41cd0f2d2570c15b11e1ff3585d27bffb5c
Derek Horton <dehort@redhat.com>
2024-12-09 13:19:24 -0600

    Compare startline and endline to determine when to add a newline

--- internal/common/ansible/runner.go
+++ internal/common/ansible/runner.go
@@ -37,8 +37,11 @@ func GetStdout(events []messageModel.PlaybookRunResponseMessageYamlEventsElem, h
 			continue
 		}
 
-		if event.Stdout != nil && len(*event.Stdout) > 0  {
-			result += *event.Stdout + "\n"
+		if event.Stdout != nil {
+			result += *event.Stdout
+			if event.EndLine > event.StartLine {
+				result += "\n"
+			}
 		}
 	}
 
