694ecf104a6a61523b3ddc259ed1b4fc1dd7e507
Ashley Young <asyoung@redhat.com>
2026-07-02 00:23:51 -0400

    RHINENG-26802; updates to fix several sast-shell syntax issues, update tekton bundles to something newer, enable mintmaker to make tekton self handling

--- event-streams/build_kafka_consumer_config.sh
+++ event-streams/build_kafka_consumer_config.sh
@@ -2,7 +2,7 @@
 
 JAAS_FILE=$1
 
-cat <<HERE > $JAAS_FILE
+cat <<HERE > "$JAAS_FILE"
 sasl.mechanism=SCRAM-SHA-512
 security.protocol=SASL_SSL
 sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required \
